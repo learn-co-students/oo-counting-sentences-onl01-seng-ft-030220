ac409a631ebb2c57ab8fb59fe3ae276d79d76c0e
Marsha Annon <marshaannon@gmail.com>
2020-03-12 00:20:09 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,34 @@
-require 'pry'
+require "pry"
 
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
-  end
+    new_array = self.split(/[.?!]/).reject {|string| string.empty?}
+    return new_array.length
+    end
+#.reject returns new array containing the items in the array for which the given block is not true
 end
