4233e92fb1a74710956a9ef478ced5de6817a424
Jessica Smith <jessikahd.smith@gmail.com>
2020-03-13 23:11:19 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,29 @@
 require 'pry'
-
 class String
-
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
-
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
-
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
-
   def count_sentences
-
+    #multiple occurences of same character at the end of sentences are squeezed into one
+    #then counts the sentences
+    self.squeeze('.?!').count('.?!')
   end
 end
